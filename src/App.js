f5340cc7b2f8c0a261f33f16d32da8334a82a8c0
risgpta <risgpta@gmail.com>
2021-05-29 18:42:02 +0530

    aremoved console

--- src/App.js
+++ src/App.js
@@ -18,11 +18,8 @@ function App() {
   function storeGuess(event) {
     let x = event.nativeEvent.offsetX;
     let y = event.nativeEvent.offsetY;
-    console.log(x);
-    console.log(y);
     guessX = x;
     guessY = y;
-    console.log("x coords: " + guessX + ", y coords: " + guessY);
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(guessX, guessY, 3, 3);
     ants.push({
@@ -33,35 +30,16 @@ function App() {
   }
 
   function draw() {
-    console.log("Draw");
-    console.log(ants);
-
     for (let i = 0; i < ants.length; i++) {
       for (let j = i + 1; j < ants.length; j++) {
         setTimeout(() => {
-          console.log(ants);
-          console.log("seconds", i * ants.length + j + 1);
           ctx.beginPath();
           ctx.moveTo(ants[i].x, ants[i].y);
           ctx.lineTo(ants[j].x, ants[j].y);
           ctx.strokeStyle =
             "#" + Math.floor(Math.random() * 16777215).toString(16);
-          console.log("#" + Math.floor(Math.random() * 16777215).toString(16));
           ctx.stroke();
         }, (i * ants.length + j + 1) * speed);
-      }
-    }
-  }
-
-  function circle() {
-    for (let i = 0; i < 1000; i++) {
-      for (let j = 0; j < 1000; j++) {
-        console.log(i, j);
-        if (Math.pow(i - 100, 2) + Math.pow(j - 100, 2) == Math.pow(100, 2)) {
-          ctx.fillStyle = "#FFFFFF";
-          ctx.fillRect(i, j, 5, 5);
-          console.log(i, j);
-        }
       }
     }
   }
